8cdc91d156c48e30586a69385dc82c9c6dc698c3
chirag <kerkarc509@gmail.com>
2025-06-18 10:05:36 +0530

    Added seed api endpoint.

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -8,6 +8,13 @@ import { LoginDto } from './dto/login.dto';
 export class AuthController {
     constructor(private authService: AuthService, private logger: AppLoggerService) { }
 
+    @Post('run-seeds')
+    async runAllSeeds() {
+        await import('../../prisma/seeders/seed');
+        await import('../../prisma/seeders/user.seed');
+        return { message: 'Seeds executed successfully' };
+    }
+
     @Post('register')
     async register(@Body() body: { email: string, password: string, name: string }) {
         try {
